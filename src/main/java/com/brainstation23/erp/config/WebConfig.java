7e3d098462201a8b3e513e30408e5ed034c1d059
Abir <abirhossain9@iut-dhaka.edu>
2023-04-26 15:50:31 +0600

    Adds allowedOriginPatterns

--- src/main/java/com/brainstation23/erp/config/WebConfig.java
+++ src/main/java/com/brainstation23/erp/config/WebConfig.java
@@ -2,20 +2,22 @@ package com.brainstation23.erp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("https://erp-production-2192.up.railway.app")
-        .allowedMethods("GET", "POST", "PUT", "DELETE")
-        .allowedHeaders("*")
-        .allowCredentials(true)
-        .maxAge(3600);
-}
+@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://erp-production-2192.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 
 
 
